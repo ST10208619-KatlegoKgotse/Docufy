b01ef32cee7de83871521ea533f0813ca9cdfbd4
Katlego Kgotse <st10208619@rcconnect.edu.za>
2023-10-28 11:23:51 +0000

    Login Doone

--- Docufy/Docufy/Login.xaml.cs
+++ Docufy/Docufy/Login.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Windows;
 
 namespace Docufy
 {
@@ -7,9 +9,71 @@ namespace Docufy
     /// </summary>
     public partial class Login : Window
     {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\lab_services_student\\Documents\\Docufy.mdf;Integrated Security=True;Connect Timeout=30";
         public Login()
         {
             InitializeComponent();
+            Loaded += (sender, args) =>
+            {
+                Wpf.Ui.Appearance.Watcher.Watch(this,
+                    Wpf.Ui.Appearance.BackgroundType.Mica,
+                    true);
+            };
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+            Registration reg = new Registration();
+            reg.Show();
+            this.Hide();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            string email = edtEmailAddress.Text;
+            string password = edtPassword.Text;
+            bool auth = SearchExistingUser(connectionString, email, password);
+            if (auth)
+                MessageBox.Show("Successful");
+            else
+                MessageBox.Show("Please try again");
+        }
+        public bool SearchExistingUser(string conn, string email, string password)
+        {
+            string query = "SELECT COUNT(*) FROM [USER] WHERE EMAIL_ADDRESS = @Email AND PASSWORD = @Password";
+            using (SqlConnection sqlConnection = new SqlConnection(conn))
+            {
+                try
+                {
+
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand(query, sqlConnection);
+                    command.Parameters.AddWithValue("@Email", email);
+                    command.Parameters.AddWithValue("@Password", password);
+
+                    int result = (int)command.ExecuteScalar();
+                    if (result == 1)
+                    {
+                        MainWindow mw = new MainWindow();
+                        mw.Show();
+                        return (true);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Please retry the information");
+                        return (false);
+                    }
+                }
+                catch (SqlException s)
+                {
+                    MessageBox.Show($"{s}");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Message: {e}");
+                }
+                return (false);
+            }
         }
     }
 }
--- Docufy/Docufy/Registration.xaml.cs
+++ Docufy/Docufy/Registration.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Windows;
 
 namespace Docufy
 {
@@ -7,6 +9,8 @@ namespace Docufy
     /// </summary>
     public partial class Registration : Window
     {
+        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\lab_services_student\\Documents\\Docufy.mdf;Integrated Security=True;Connect Timeout=30";
+
         public Registration()
         {
             InitializeComponent();
@@ -17,6 +21,48 @@ namespace Docufy
                     Wpf.Ui.Appearance.BackgroundType.Mica,
                     true);
             };
+        }
+
+        private void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            Login log = new Login();
+            log.Show();
+            this.Hide();
+        }
+        public bool SearchExistingUser(string conn, string email, string password)
+        {
+            string query = "SELECT COUNT(*) FROM [USER] WHERE EMAIL_ADDRESS = @Email";
+            using (SqlConnection sqlConnection = new SqlConnection(conn))
+            {
+                try
+                {
+
+                    sqlConnection.Open();
+                    SqlCommand command = new SqlCommand(query, sqlConnection);
+                    command.Parameters.AddWithValue("@Email", email);
+                    int result = (int)command.ExecuteScalar();
+                    if (result == 1)
+                    {
+                        MessageBox.Show($"This user exists");
+                        return (true);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Please Login");
+                        return (false);
+                    }
+                }
+                catch (SqlException s)
+                {
+                    MessageBox.Show($"{s}");
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show($"Message: {e}");
+                }
+                return (false);
+            }
+
         }
     }
 }
